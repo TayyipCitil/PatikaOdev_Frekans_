8e8d0dc2ef30dca52f1c8c38266b6ef2b72617a0
= <=>
2024-07-09 23:37:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //dizinin hangi elemanı kaç kez tekrar edilmiş yani frekansları kaçtır bulan program
+        int[] list={1,1,1,2,2,2,2,3,3,4,5};
+    }
+    static void frekans(int[] list){
+
+
+    }
+}
