057ee33b9aa9a6494d0dbc742718396943d0427f
= <=>
2024-07-10 20:38:55 +0300

    version 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //dizinin hangi elemanı kaç kez tekrar edilmiş yani frekansları kaçtır bulan program
-        int[] list={1,1,1,2,2,2,2,3,3,4,5};
+        int[] list = {1, 1, 1, 1, 4, 6, 3, 4, 8, 20, 10, 10, 10, 20, 20, 20, 4, -3, -3};
+        int[][] matrix = repeatingNum(list);//matrisin ilk satırına listemizi sıralayaaarak yazdık ikinci satıra tekrar sayılarını yazdık
+        int i = 0;
+        while (i < matrix[0].length) {
+            System.out.println("Eleman: " + matrix[0][i] + " Tekrar sayısı: " + matrix[1][i]);
+            i += matrix[1][i];
+        }
     }
-    static void frekans(int[] list){
+    static int[][] repeatingNum(int[] list) {
+        int[][] matrix = new int[2][list.length];
+        matrix[0] = list;
+        Arrays.sort(list);
+        for (int i = 0; i < list.length; i++) {
+            int counter = 0;
+            for (int element : list) {
+                if (list[i] == element) {
+                    counter++;
+                }
+            }
+            matrix[1][i] = counter;
+        }
+        return matrix;
+    }
 
 
-    }
 }
